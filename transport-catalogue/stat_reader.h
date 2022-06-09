bdb68aa443f2de0353131fe06b6622ab46ec1ba0
kalchmiitta <98318357+kalchmiitta@users.noreply.github.com>
2022-06-09 19:42:30 +0300

    first_version

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,12 @@
-// место для вашего кода
+#pragma once
+
+#include <ostream>
+#include "transport_catalogue.h"
+
+namespace catalogue::output {
+    void Responses (std::istream &in, std::ostream &out, const TransportCatalogue &catalogue);
+    
+    void BusResponse (std::ostream &out, string_view text, const TransportCatalogue &catalogue);
+    
+    void StopResponse (std::ostream &out, string_view stop_name, const TransportCatalogue &catalogue);
+}
