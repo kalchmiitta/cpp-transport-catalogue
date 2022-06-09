e0548d337e6f087e464c177dd40470cef9111cc0
kalchmiitta <98318357+kalchmiitta@users.noreply.github.com>
2022-06-09 19:41:38 +0300

    first_version

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,10 @@
-// место для вашего кода
+#include "transport_catalogue.h"
+#include "input_reader.h"
+#include "stat_reader.h"
+
+
+int main(){
+    catalogue::TransportCatalogue catalogue;
+    catalogue::input::FillDatabase(std::cin, catalogue);
+    catalogue::output::Responses(std::cin, std::cout, catalogue);
+}
