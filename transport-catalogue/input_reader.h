570baaefff0e2348c14ed485d08b56dbd6471f4d
kalchmiitta <98318357+kalchmiitta@users.noreply.github.com>
2022-06-09 19:41:01 +0300

    first_version

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,1 +1,15 @@
-// место для вашего кода
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#include "transport_catalogue.h"
+
+namespace catalogue::input{
+    void FillDatabase(std::istream &in, TransportCatalogue& catalogue);
+    
+    std::pair<string, vector<std::pair<string, int>>> ParseStop(string_view text, TransportCatalogue& catalogue);
+    vector<std::pair<string, int>> ParseDistancesForStop (string_view text);
+    
+    void ParseBus(string_view text, TransportCatalogue& catalogue);
+}
